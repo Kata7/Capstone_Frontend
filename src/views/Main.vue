51b75e4c901dcc0c80575da0678eba2716dd07e4
Austin Moran <a.moran3257@gmail.com>
2019-04-06 10:19:02 -0600

    working on the main image display view

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="main">
     <div class="images">
-      <p>Pictures of food go here</p>
-      <img src="/#"/>
+      <p>Restaurant Name?</p>
+      <img src="https://www.gannett-cdn.com/-mm-/1fecae5856e58374cc9e1c0fd6dcc3c6aae79d4e/c=0-293-5760-3547/local/-/media/2018/07/17/IAGroup/DesMoines/636674359927753055-0717-NEW-STATEFAIR-FOODS-00029.jpg?width=3200&height=1680&fit=crop"/>
     </div>
     <div class="description-short">
-      <p>info goes here</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
   </div>
 </template>
